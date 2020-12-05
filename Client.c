2e63adb312a748e7841cb9c3c429eef749a68481
Paste <raj49274@gmail.com>
2020-12-05 19:50:00 +0530

    implemented the view

--- Client.c
+++ Client.c
@@ -1,3 +1,4 @@
+// Client side C/C++ program to demonstrate Socket programming
 #include <stdio.h>
 #include <sys/socket.h>
 #include <stdlib.h>
@@ -5,7 +6,10 @@
 #include <unistd.h>
 #include <string.h>
 
-int main(int argc, char *argv[])
+void dashboard();
+void view();
+
+int main(int argc, char const *argv[])
 {
     int sockfd, n;
     char buffer[1024];
@@ -24,11 +28,6 @@ int main(int argc, char *argv[])
         printf("socket error\n");
         exit(1);
     }
-    else
-    {
-        printf("Socket successfully created\n");
-    }
-    
 
     // flushing the data in servaddr
     bzero(&servaddr, sizeof(servaddr));
@@ -36,7 +35,7 @@ int main(int argc, char *argv[])
     // giving input to struct sockaddr_in
     servaddr.sin_family = AF_INET;
     // htons() is host to network short
-    servaddr.sin_port = htons(8000); // giving port no - 80 for httpserver
+    servaddr.sin_port = htons(9000); // giving port no - 80 for httpserver
     // now using inet_pton() ("presentation to numric") function to input in right format
     if (inet_pton(AF_INET, argv[1], &servaddr.sin_addr) <= 0)
     {
@@ -50,13 +49,67 @@ int main(int argc, char *argv[])
         printf("connect error\n");
         exit(1);
     }
-    else
+
+    while (1)
     {
-        printf("Connection established with the server");
+
+        dashboard();
+        printf("       Enter your choice: ");
+        char temporary[5];
+        scanf("%[^\n]%*c", temporary);
+        bzero(buffer, sizeof(buffer));
+        strcpy(buffer, temporary);
+
+        write(sockfd, buffer, sizeof(buffer));
+
+        // now the time to select the game option
+        if (strcmp(buffer, "1") == 0)
+        {
+            bzero(buffer, sizeof(buffer));
+            read(sockfd, buffer, sizeof(buffer));
+            view(buffer);
+        }
+        else if (strcmp(buffer, "2") == 0)
+        {
+        }
+        else
+        {
+        }
+        sleep(5);
     }
-    
-    
-    
 
     return 0;
 }
+
+// Views of the game
+
+void dashboard()
+{
+    system("clear");
+    printf("\n\n\n");
+    printf("%25c%s\n\n", ' ', "welcome to Tic-Tac-Toe Game");
+    printf("%28c%s\n", ' ', "1. Create the Room");
+    printf("%28c%s\n", ' ', "2. Join the Room");
+    printf("%28c%s\n", ' ', "3. Exit the Game");
+    printf("\n\n");
+}
+
+
+void view(char *num)
+{
+    system("clear");
+    printf("\n\n\n\n");
+    printf("%25c     |      |      \n", ' ');
+    printf("%25c  %c  |   %c  |  %c   \n", ' ', num[0], num[1], num[2]);
+    printf("%25c_____|______|______\n", ' ');
+    printf("%25c     |      |      \n", ' ');
+    printf("%25c  %c  |   %c  |  %c   \n", ' ', num[3], num[4], num[5]);
+    printf("%25c_____|______|_____\n", ' ');
+    printf("%25c     |      |      \n", ' ');
+    printf("%25c  %c  |   %c  |  %c   \n", ' ', num[6], num[7], num[8]);
+    printf("%25c     |      |      \n", ' ');
+    printf("\n\n");
+}
+
+
+// Opting the to host 
--- Tic_Tac_Toe_Server.c
+++ Tic_Tac_Toe_Server.c
@@ -1,90 +1,120 @@
-/**
- * Team Project
- * 
- * Team Members:
- * 1) Aditya Raj
- * 2) Tanmay 
- * 3) Nisarg
-*/
-
-/**
- * Project Content
- * 1) TCP server Implemented
- * 2)
- * 3)
- * 
-*/
-
-
-// Module needed
+// Server side C/C++ program to demonstrate Socket programming
 #include <unistd.h>
 #include <stdio.h>
 #include <sys/socket.h>
 #include <stdlib.h>
 #include <netinet/in.h>
 #include <string.h>
+#include <time.h>
 
-/**
- * Function Declaration
-*/
+// function declaration
+void hostTheGame();
+void getDate();
 
-
-
-/* Driver Function */
-int main(int argc, char *argv[])
+int main(int argc, char const *argv[])
 {
-    int sockfd, connfd;
-    struct sockaddr_in servaddr, cliaddr;
+    int listenfd, connfd;
+    char buffer[1024];
+    struct sockaddr_in servaddr;
+    time_t ticks;
 
-    // socket formation and error checking
-    if((sockfd= socket(AF_INET, SOCK_STREAM, 0)) < 0){
-        printf("Error: Socket creation failed...");
+    // creating listening Socket
+    if ((listenfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+    {
+        printf("socket error\n");
         exit(1);
-    }else{
-        printf("Socket successfully created...\n");
     }
 
+    //flusing the value os servaddr
     bzero(&servaddr, sizeof(servaddr));
-    
-    // passing data to structure sockaddr_in
+
+    // giving input to struct sockaddr_in
     servaddr.sin_family = AF_INET;
+    //
     servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
-    servaddr.sin_port = htons(8000);
+    // htons() is host to network short
+    servaddr.sin_port = htons(9000); // giving port no - 80 for httpserver
 
-
-    // Binding the created socket to the IP address
-    if((bind(sockfd, (struct sockaddr *)&servaddr, sizeof(servaddr))) == -1){
-        printf("Error: Socket bind failed...\n");
+    if (bind(listenfd, (struct sockaddr *)&servaddr, sizeof(servaddr)) < 0)
+    {
+        printf("bind error\n");
         exit(1);
-    }else{
-        printf("Socket successfully binded...\n");
     }
 
-
-    // Calling listen call to put server in listen state
-    if((listen(sockfd, 10)) != 0){
-        printf("Error: Listen failed...\n");
+    //starting listening on created Socket
+    if (listen(listenfd, 5) < 0)
+    {
+        printf("listen error\n");
         exit(1);
-    }{
-        printf("Server listening...\n");
     }
 
-    ////////////////////////////////////////////////////
-    /// Accepting the client request
-    int len = sizeof(cliaddr);
-    if(connfd = accept(sockfd, (struct sockaddr *)&cliaddr, &len) < 0){
-        printf("Error: Server accept failed...\n");
-        exit(1);
-    }else{
-        printf("Server accepted the client");
+    // creating new socket to accept the connection
+    for (;;)
+    {
+        if ((connfd = accept(listenfd, (struct sockaddr *)NULL, NULL)) < 0)
+        {
+            printf("connection error\n");
+            exit(1);
+        }
+        while (1)
+        {
+
+            bzero(buffer, sizeof(buffer));
+            read(connfd, buffer, sizeof(buffer));
+
+            if (strcmp(buffer, "1") == 0)
+            {
+                printf("1 is received\n");
+                hostTheGame(connfd);
+            }
+            else if (strcmp(buffer, "2") == 0)
+            {
+                printf("2 is received\n");
+                getDate(connfd);
+            }
+            else if (strcmp(buffer, "3") == 0)
+            {
+                printf("3 is received\n");
+                write(connfd, buffer, sizeof(buffer));
+                exit(0);
+            }
+            else
+            {
+                close(connfd);
+            }
+        }
     }
-
-    ///////////
-    
-    
-   
-    close(sockfd);
-
-    return 0;
 }
 
+// function to process the incoming request
+void hostTheGame(int socket)
+{
+    char Buffer[256];
+    char temp[20];
+    bzero(Buffer, sizeof(Buffer));
+    strcpy(Buffer, "123456789");
+    write(socket, Buffer, sizeof(Buffer));
+}
+
+// function to show date
+void getDate(int socket)
+{
+    time_t t;
+    time(&t);
+    char Buffer[256];
+    char result[50];
+    char temp[20];
+    bzero(result, sizeof(result));
+    struct tm dt = *localtime(&t);
+
+    sprintf(temp, "%d/", dt.tm_mday);
+    strcat(result, temp);
+    sprintf(temp, "%d/", dt.tm_mon + 1);
+    strcat(result, temp);
+    sprintf(temp, "%d", dt.tm_year + 1900);
+    strcat(result, temp);
+
+    bzero(Buffer, sizeof(Buffer));
+    strcpy(Buffer, result);
+    write(socket, Buffer, sizeof(Buffer));
+}
